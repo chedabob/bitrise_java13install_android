e656de8e04711aba878a439ce196a77a7727017b
Matt Malone <mattm@apadmi.com>
2020-08-15 13:31:33 +0100

    Bit more debug output

--- step.sh
+++ step.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo "Installing OpenJDK 13"
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get -y install openjdk-13-jdk
+
+echo "New Java version"
+which java
+java -version

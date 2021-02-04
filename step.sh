c62a4eaa1cc29b001710d7c1dccf2ab95c6ac108
mattmapadmi <40175381+mattmapadmi@users.noreply.github.com>
2021-02-04 11:51:39 +0000

    Update step.sh

--- step.sh
+++ step.sh
@@ -4,6 +4,8 @@ echo "Installing OpenJDK 13"
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get -y install openjdk-13-jdk
+sudo update-java-alternatives --jre-headless --set java-1.13.0-openjdk-amd64
+sudo update-alternatives --set javac /usr/lib/jvm/java-13-openjdk-amd64/bin/javac
 
 echo "New Java version"
 which java

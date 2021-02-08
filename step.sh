43e1d170df0e7e623a79fe90ea46eaf454891386
mattmapadmi <40175381+mattmapadmi@users.noreply.github.com>
2021-02-08 11:16:47 +0000

    Update step.sh

--- step.sh
+++ step.sh
@@ -6,6 +6,7 @@ sudo apt-get update
 sudo apt-get -y install openjdk-13-jdk
 sudo update-java-alternatives --jre-headless --set java-1.13.0-openjdk-amd64
 sudo update-alternatives --set javac /usr/lib/jvm/java-13-openjdk-amd64/bin/javac
+sudo update-alternatives --set java /usr/lib/jvm/java-13-openjdk-amd64/bin/java
 
 echo "New Java version"
 which java
